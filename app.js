f8647bf57b4e887f2a668f314e49622a00ce3635
Jeremy <jeremy.stegall77@gmail.com>
2020-12-10 11:54:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 const exphbs = require('express-handlebars');
+const dotenv = require('dotenv');
+const path = require('path');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = 3000;
 
