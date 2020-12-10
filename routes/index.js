9516258ef46af30ce2030024939c93b973039a1b
Jeremy <jeremy.stegall77@gmail.com>
2020-12-10 12:48:13 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res)=>{
     res.send('Login Page');
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
-router.get('/dashboard', (req,res)=>{
-    res.send('Dashboard')
+router.get('/dashboard', (req, res)=>{
+    res.render('dashboard')
 })
 
 
