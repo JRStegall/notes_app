ba769b61ca160e1c9d46b5554f9400e3cead8a6d
Jeremy <jeremy.stegall77@gmail.com>
2020-12-14 14:16:55 -0500

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,19 @@
+const e = require('express');
 const express = require('express');
 const { ensureAuth } = require('../middleware/auth');
 const router = express.Router();
 const Note = require('../models/Note');
+
+router.get('/add', ensureAuth,(req, res)=>{
+    res.render('/notes/add');
+})
+
+router.post('/add', ensureAuth,async(req,res)=>{
+    try{
+        req.body.user = req.user._id;
+        const note = await Note.create(req.body);
+        res.redirect('/dsahboard');
+    }catch(err){
+        console.log(err);
+    }
+})
