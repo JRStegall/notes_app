e6b6ff2f5c22795bfe0a8edfc2733a983958b5c1
Jeremy <jeremy.stegall77@gmail.com>
2020-12-16 12:03:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
 const passport = require('passport');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
-const methodOverride = require('method-override');
-const { delete } = require('request');
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +14,18 @@ const PORT = 3000;
 app.use(require('morgan')('dev'));
 
 //BODY PARSER
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//OVERRIDE MIDDLEWARE
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        //LOOK IN URLENCODED POST BODIES AND DELETE IT
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 
 //PASSPORT CONFIG
 require('./config/passport')(passport);
@@ -26,7 +35,7 @@ dotenv.config();
 connectDB();
 
 //HANDLEBARS
-app.engine('.hbs', exphbs({defaultLayout: 'main', 'extname' : '.hbs'}));
+app.engine('.hbs', exphbs({ defaultLayout: 'main', 'extname': '.hbs' }));
 app.set('view engine', '.hbs');
 
 //EXPRESS SESSIONS MIDDLEWARE
@@ -34,27 +43,19 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection : mongoose.connection }),
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
 }))
 
 //PASSPORT MIDDLEWARE
 app.use(passport.initialize());
 app.use(passport.session());
 
-//OVERRIDE MIDDLEWARE
-app.use(methodOverride(function(req, res){
-    if (req.body && typeof req.body === 'object' && '_method' in req.body){
-        //LOOK IN URLENCODED POST BODIES AND DELETE IT
-        var method = req.body._method;
-        delete req.body._method;
-        return method;
-    }
-}));
+
 
 //ROUTES
-app.use('/',require('./routes/index'));
-app.use('/auth',require('./routes/auth'));
-app.use('/notes',require('./routes/notes'));
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/notes', require('./routes/notes'));
 
 
 
