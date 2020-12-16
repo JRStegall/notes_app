890643fc1cff401803999b1134a842b10d2536f2
Jeremy <jeremy.stegall77@gmail.com>
2020-12-16 10:33:39 -0500

    Update

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const session = require('express-session');
 const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo')('session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 const app = express();
 const PORT = 3000;
+
+app.use(require('morgan')('dev'));
+
+//BODY PARSER
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //PASSPORT CONFIG
 require('./config/passport')(passport);
@@ -36,6 +42,7 @@ app.use(passport.session());
 //ROUTES
 app.use('/',require('./routes/index'));
 app.use('/auth',require('./routes/auth'));
+app.use('/notes',require('./routes/notes'));
 
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,20 @@
-const e = require('express');
 const express = require('express');
 const { ensureAuth } = require('../middleware/auth');
 const router = express.Router();
 const Note = require('../models/Note');
 
-router.get('/add', ensureAuth,(req, res)=>{
-    res.render('/notes/add');
+router.get('/add',ensureAuth,(req, res)=>{
+    res.render('notes/add');
 })
 
-router.post('/add', ensureAuth,async(req,res)=>{
+router.post('/add', ensureAuth, async(req,res)=>{
     try{
         req.body.user = req.user._id;
         const note = await Note.create(req.body);
-        res.redirect('/dsahboard');
+        res.redirect('/dashboard');
     }catch(err){
         console.log(err);
     }
 })
+
+module.exports = router;
